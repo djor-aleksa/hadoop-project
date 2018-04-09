419a23acee21f7892379a5336e91577ad84cddc6
djor-aleksa <36139188+djor-aleksa@users.noreply.github.com>
2018-04-09 23:24:54 +0200

    Update TDCPartitioner.java

--- src/main/java/timeDistanceCorrelation/TDCPartitioner.java
+++ src/main/java/timeDistanceCorrelation/TDCPartitioner.java
@@ -7,7 +7,7 @@ import org.apache.hadoop.mapreduce.Partitioner;
 public class TDCPartitioner extends Partitioner<Text, FloatWritable>{
 
     @Override
-    public int getPartition(Text text, FloatWritable floatWritable, int i) {
+    public int getPartition(Text key, FloatWritable value, int i) {
         return 0;
     }
 }
